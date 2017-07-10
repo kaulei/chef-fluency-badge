579b7cb0ada563240dab039d70ad5053f836f9a6
tobler <tobler@example.com>
2017-07-10 22:34:05 +0000

    Add basic Apache cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "alei"
-client_key               "#{current_dir}/alei.pem"
-chef_server_url          "https://kaulei-gmail-com1.mylabserver.com/organizations/cyren"
+node_name                "kaulei"
+client_key               "#{current_dir}/kaulei.pem"
+chef_server_url          "https://tobler1.mylabserver.com/organizations/cyren"
 cookbook_path            ["#{current_dir}/../cookbooks"]
